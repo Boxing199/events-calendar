a529a70e2bea124542ee061ca73854622e78f924
Boxing199 <virkolomeo@gmail.com>
2019-07-15 00:06:37 +0300

    Updates

---
+++ precache-manifest.2da97bfa91e7e3d8504d4ffef0b2b18c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98700157a138299ac0a740ffab0ac94",
+    "url": "/events-calendar/index.html"
+  },
+  {
+    "revision": "f1deffab2ce27c26be59",
+    "url": "/events-calendar/static/css/main.e8ebd58a.chunk.css"
+  },
+  {
+    "revision": "48d5d6c2dcad30564736",
+    "url": "/events-calendar/static/js/2.5ffe231e.chunk.js"
+  },
+  {
+    "revision": "f1deffab2ce27c26be59",
+    "url": "/events-calendar/static/js/main.c4441f52.chunk.js"
+  },
+  {
+    "revision": "e0f2172b52f3aa06cc40",
+    "url": "/events-calendar/static/js/runtime~main.89887397.js"
+  }
+]);
