fdfd9a4ad0e3dc74808cddad5bb8d68ca8261f4c
Boxing199 <virkolomeo@gmail.com>
2019-07-24 12:57:27 +0300

    Updates

--- precache-manifest.886f322f423f26d80cf85bff3e08529d.js
+++ precache-manifest.886f322f423f26d80cf85bff3e08529d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af3cf4ea1e9b2747b74aac0b15e666be",
+    "revision": "49daf6ab5294029561f1661177d98989",
     "url": "/events-calendar/index.html"
   },
   {
-    "revision": "50af57d4dd74051ecc1a",
+    "revision": "ba1fbfdb9b98849a339e",
     "url": "/events-calendar/static/css/main.36cb7979.chunk.css"
   },
   {
-    "revision": "9922444bc44fcaa1d389",
-    "url": "/events-calendar/static/js/2.1b5e08b6.chunk.js"
+    "revision": "2da159a2e06e1369d015",
+    "url": "/events-calendar/static/js/2.25b8de7e.chunk.js"
   },
   {
-    "revision": "50af57d4dd74051ecc1a",
-    "url": "/events-calendar/static/js/main.ffdacd20.chunk.js"
+    "revision": "ba1fbfdb9b98849a339e",
+    "url": "/events-calendar/static/js/main.5d7e3fd8.chunk.js"
   },
   {
     "revision": "e0f2172b52f3aa06cc40",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/events-calendar/precache-manifest.58953d12a70a5c1aaef084332485c3d3.js"
+  "/events-calendar/precache-manifest.886f322f423f26d80cf85bff3e08529d.js"
 );
 
 self.addEventListener('message', (event) => {
